9d1e1b55f531fdc6cb684d1b203c968c3c0994d6
Abdülsamet YILMAZ <mr.asmtylmz@gmail.com>
2023-02-03 02:21:10 +0300

    Hesap Makinesi
    Kullanıcıdan değer alarak Basit konsol uygulaması

---
+++ hesap makinesi/Program.cs
@@ -0,0 +1,26 @@
+﻿int sayi1;
+int sayi2;
+string op;
+
+Console.WriteLine("Lütfen ilk değeri giriniz : ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :");
+op = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Lütfen ikinci değeri giriniz : ");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+switch (op)
+{
+    case "+": Console.WriteLine($"{sayi1} + {sayi2} toplamı = {sayi1 + sayi2}"); break;
+
+    case "-": Console.WriteLine($"{sayi1} - {sayi2} farkı = {sayi1 - sayi2}"); break;
+
+    case "*": Console.WriteLine($"{sayi1} * {sayi2} çarpımı = {sayi1 * sayi2}"); break;
+
+    case "/": Console.WriteLine($"{sayi1} / {sayi2} bölümü = {sayi1 / sayi2}"); break;
+
+}
